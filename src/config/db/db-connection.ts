10a9b7d379fc29961324fae604b2b9e7e4e5421f
Alexis Orellana <1005alexis@gmail.com>
2024-07-23 15:02:40 -0600

    feature: KAN-37 Implement Global Logger

--- src/config/db/db-connection.ts
+++ src/config/db/db-connection.ts
@@ -1,12 +1,26 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
+import { logger } from '../../utils/logger';
+import { log } from 'console';
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASS!, {
-  host: process.env.DB_HOST,
-  dialect: 'mariadb',
-  // logging: (...msg) => console.log("==DB Query: ", msg)
-});
+export const sequelize = new Sequelize(
+  process.env.DB_NAME!,
+  process.env.DB_USER!,
+  process.env.DB_PASS!,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mariadb',
+    // logging: (...msg) => console.log("==DB Query: ", msg)
+  }
+);
 
-export default sequelize;
+export const connectAppToDatabase = async () => {
+  try {
+    await sequelize.authenticate();
+    logger('info', 'Connection has been established successfully.', 'db-connection.ConnectAppToDatabase');
+  } catch (error) {
+    logger('error', `Unable to connect to the database: ${error}`, 'db-connection.ConnectAppToDatabase');
+  }
+};
